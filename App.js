6e0151a0b3e5fd330e9467691f1b8a8e19dd7a9c
mc9ry <mc9ry@virginia.edu>
2024-07-20 20:48:02 -0400

    setup env, setup expo, beginning dev

--- App.js
+++ App.js
@@ -4,7 +4,11 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.lyrics}>
+        My Soyan bad, my Soyan hood My Soyan do stuff that your chick wish she
+        could My Soyan bad, badder than yours My Soyan do stuff that I can’t
+        even put in words
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
